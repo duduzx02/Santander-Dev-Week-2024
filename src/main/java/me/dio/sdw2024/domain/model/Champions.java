e0954c8f87c6f4c4e9e6244d9d28b8ce7ca462a0
Eduardo Lima <duduzx02@gmail.com>
2024-03-26 01:59:28 -0300

    domain com model e ports: criados

---
+++ src/main/java/me/dio/sdw2024/domain/model/Champions.java
@@ -0,0 +1,13 @@
+package me.dio.sdw2024.domain.model;
+
+public record Champions(
+        Long id,
+        String name,
+        String role,
+        String lore,
+        String imageUrl
+) {
+
+
+
+}
